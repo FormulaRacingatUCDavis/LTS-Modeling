c53aeb27de00a4c9cefd112c432e5f64f668a50c
bechrist <40005251+bechrist@users.noreply.github.com>
2020-12-28 21:42:26 -0800

    Random Stuffs

--- Steady State LTS/FRUCDSteadyStateLTS.m
+++ Steady State LTS/FRUCDSteadyStateLTS.m
@@ -3,18 +3,11 @@ clc; clear; close all;
 Event = load( ['C:\Users\Authorized User\Documents\Clubs\GitHub\LTS-Modeling\', ...
                'Track Profiling\Analyses\2015-18_Lincoln_AutoX.mat'] );
 
-
-Apex = islocalmin( abs(Event.Spline.Radius) );
+Apex = islocalmin( abs(Event.Spline.Radius(:,2)), 'MinProminence', 2 );
+Apex = Apex & Event.Spline.IsCorner;
 
 figure;
-plot( Event.Spline.Distance, abs(Event.Spline.Radius) ); hold on;
-plot( Event.Spline.Distance(Apex), abs(Event.Spline.Radius(Apex)), 'ro' );
-ylim([-250 250]);
-
-figure
-imshow( Event.Image.Raw ); hold on;
-scatter( Event.Spline.Pixels( Event.Spline.Radius>100,1 ), Event.Spline.Pixels( Event.Spline.Radius>100,2 ), 2, 'k' )
-scatter( Event.Spline.Pixels( Event.Spline.Radius<100,1 ), Event.Spline.Pixels( Event.Spline.Radius<100,2 ), 2, 'r' )
-diff( find( Event.Spline.Radius > 250 ) )
-
+plot( Event.Spline.Distance, abs(Event.Spline.Radius(:,2)) ); hold on;
+plot( Event.Spline.Distance(Apex), abs(Event.Spline.Radius(Apex ,2)), 'ro' );
+ylim([0 150]);
 
