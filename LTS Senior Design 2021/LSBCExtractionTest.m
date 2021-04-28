543a7fd0e780080b2f78075d112d4bc627eee7a3
bechrist <40005251+bechrist@users.noreply.github.com>
2021-04-28 13:31:28 -0700

    Update LSBCExtractionTest.m

--- LTS Senior Design 2021/LSBCExtractionTest.m
+++ LTS Senior Design 2021/LSBCExtractionTest.m
@@ -4,20 +4,18 @@ clc; clear; close all;
 
 PE = load('TestPE.mat');
 
-<<<<<<< HEAD
 %% Performance Envelope Interpolation
 % PE.State(SteerIdx,BodySlipIdx,SpeedIdx,LongAccIdx)
 
 %PE.Interp.LatAcc = interpn( PE.BodySlip, PE.Steer, PE.Speed, PE.LongAcc, PE.LatAcc );
 %PE.Interp.YawAcc = interpn( PE.BodySlip, PE.Steer, PE.Speed, PE.LongAcc, PE.YawAcc );
-=======
+
 %% LAS Volumetric Interpolation
 %LatAcc = griddedInterpolant( LAS.BodySlip, LAS.Steer, LAS.Speed, LAS.LongAcc, LAS.LatAcc );
 %YawAcc = griddedInterpolant( LAS.BodySlip, LAS.Steer, LAS.Speed, LAS.LongAcc, LAS.YawAcc );
 
 %% LAS 4D Shelling
 %LAS.ShellIdx = 
->>>>>>> 48efc1726c0aad9d773d7380229d4d1bf7af6179
 
 %% LAS Shelling
 % LAS.State(:,2*(SteerIdx+BodySlipIdx-2),SpeedIdx,LongAccIdx) % Dimensionality
@@ -27,7 +25,7 @@ LAS.ShellIdx = [[(1:size(PE.BodySlip, 1))                            ; ones(1, s
                 [ones(1, size(PE.BodySlip, 2)-2)                     ;(size(PE.BodySlip, 2)-1:-1:2)                        ]] ;         
 
       
-LAS.ShellIdx = repmat( 
+LAS.ShellIdx = repmat(1,1,1);
 LAS.BodySlip = PE.BodySlip( sub2ind( size(PE.BodySlip), LAS.ShellIdx(1,:), LAS.ShellIdx(2,:), ones(1,length(LAS.ShellIdx)), ones(1,length(LAS.ShellIdx)) ) ); 
             
 %% LSBS Extraction (Indexing LongAcc == 0)
@@ -36,13 +34,9 @@ LSBS.Speed = LAS.Speed( PE.LongAcc == 0 );
 LSBS.LatAcc = LAS.LatAcc( PE.LongAcc == 0 );
 LSBS.YawAcc = LAS.YawAcc( PE.LongAcc == 0 );
 
-<<<<<<< HEAD
 scatter3( LSBS.Speed, LSBS.LatAcc, LSBS.YawAcc, 'k.' );
 
 %% LSBC Extraction (Indexing Speed = BaseSpeed from LSBS)
 
 %% GGV-V Extraction (Slicing where YawAcc = 0)
 
-
-=======
->>>>>>> 48efc1726c0aad9d773d7380229d4d1bf7af6179
