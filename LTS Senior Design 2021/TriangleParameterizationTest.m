04e229ef2b1f25149f09e29434fc33ae75db44e2
bechrist <40005251+bechrist@users.noreply.github.com>
2021-04-30 22:56:13 -0700

    Update TriangleParameterizationTest.m

--- LTS Senior Design 2021/TriangleParameterizationTest.m
+++ LTS Senior Design 2021/TriangleParameterizationTest.m
@@ -1,8 +1,9 @@
 clc; clear; close all;
 
-%% Triangle Parameterization Test
+%% Simplex Parameterization Test
 % Parameterizing triangle by two edges
 
+%% 2D Test (Triangle)
 p = [-2, 4, 7; ...
      -3, 1,-4];
 
@@ -15,4 +16,28 @@ Data = p(:,1) + X1 .* v(:,1) + X2 .* v(:,2);
 X_Data = squeeze(Data(1,:,:));
 Y_Data = squeeze(Data(2,:,:));
 
-scatter( X_Data(X1+X2<=1), Y_Data(X1+X2<=1), 'k.' )
+figure;
+scatter( X_Data(X1+X2<=1), Y_Data(X1+X2<=1), 'k.' );
+
+%% 3D Test (Tetrahedron)
+p = [-2, 4, 7, 5; ...
+     -3, 1,-4, 2; ...
+      0,-3, 4, 2];
+
+v = p(:,2:end) - p(:,1);
+
+n = 5;
+X1 = ones( 1,n,n,n );
+X2 = ones( 1,n,n,n );
+X3 = ones( 1,n,n,n );
+
+[X1(1,:,:,:), X2(1,:,:,:), X3(1,:,:,:)] = ndgrid( linspace(0,1,n), linspace(0,1,n), linspace(0,1,n) );
+
+Data = p(:,1) + X1 .* v(:,1) + X2 .* v(:,2) + X3 .* v(:,3); 
+
+X_Data = squeeze(Data(1,:,:));
+Y_Data = squeeze(Data(2,:,:));
+Z_Data = squeeze(Data(3,:,:));
+
+figure;
+scatter3( X_Data(X1+X2+X3<=1), Y_Data(X1+X2+X3<=1), Z_Data(X1+X2+X3<=1), 'k.' );
