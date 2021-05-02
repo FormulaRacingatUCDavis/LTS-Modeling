cc03768a88692cb9f86abf2dcce3cab8f7b6dc2d
DLaSpada <74581966+DLaSpada@users.noreply.github.com>
2021-05-02 12:46:54 -0700

    Update CurvatureTrace.m

--- Track Profiling/CurvatureTrace.m
+++ Track Profiling/CurvatureTrace.m
@@ -5,11 +5,8 @@ s=1;
 
 cy = 2 * Spline.Coeff.c(s, 2); % d^2y/du^2
 cx = 2 * Spline.Coeff.c(s, 1); % d^2x/du^2
-c = cy/cx; % d^2y/dx^2
-if cx == 0
-    c = 0;
-end
-Curv = [c, t];
+c = sqrt( cx^2 + cy^2 ); % Curvature
+Curv = [c, 0];
 while t <= Spline.Distance(end)
     while t > Spline.Distance(s*1000)
         s=s+1;
@@ -18,7 +15,7 @@ while t <= Spline.Distance(end)
         (Spline.Distance(s*1000) - Spline.Distance((s-1)*1000 +1));
     cy = 2 * Spline.Coeff.c(s, 2) + 6 * Spline.Coeff.d(s,2) * u;
     cx = 2 * Spline.Coeff.c(s, 1) + 6 * Spline.Coeff.d(s,1) * u;
-    c = cy/cx;
+    c = sqrt( cx^2 + cy^2 );
     
     Curv = [Curv; c, t];
     t = t + resolution;
